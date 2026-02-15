7e85054a9debc72c9c79422014c9477dd93771b7
damien <damiengabrielle@gmail.com>
2026-02-15 13:24:20 +0100

    fix github action's ci

--- src/clsx.erl
+++ src/clsx.erl
@@ -14,7 +14,7 @@ to_str(List) when is_list(List) -> List.
 run([], Acc) ->
     Acc;
 run([{_, False} | Tail], Acc)
-  when False =:= false; False =:= null; False =:= undefined;
+  when not False; False =:= null; False =:= undefined;
        False =:= 0; False =:= []; False =:= '' ->
     run(Tail, Acc);
 run([{Key, Predicate} | Tail], Acc) when is_function(Predicate, 0) ->
@@ -26,21 +26,24 @@ run([{Key, Predicate} | Tail], Acc) when is_function(Predicate, 0) ->
 run([{Key, _} | Tail], Acc) ->
     run(Tail, [Key | Acc]);
 run([False | Tail], Acc)
-  when False =:= false; False =:= null; False =:= undefined;
+  when not False; False =:= null; False =:= undefined;
        False =:= 0; False =:= []; False =:= '' ->
     run(Tail, Acc);
 run([Atom | Tail], Acc) when is_atom(Atom) ->
     run(Tail, [atom_to_list(Atom) | Acc]);
 run([List | Tail], Acc) when is_list(List) ->
-    case catch string:trim(List) of
-        [] -> run(Tail, Acc);
-        {'EXIT', _} -> run(Tail, run(List, []) ++ Acc);
-        Key -> run(Tail, [Key | Acc])
+    try
+        case string:trim(List) of
+            [] -> run(Tail, Acc);
+            Key -> run(Tail, [Key | Acc])
+        end
+    catch
+        _:_ -> run(Tail, run(List, []) ++ Acc)
     end;
-run([{true, If_true, _} | Tail], Acc) ->
-    run(Tail, [If_true | Acc]);
-run([{false, _, If_false} | Tail], Acc) ->
-    run(Tail, [If_false | Acc]);
+run([{true, IfTrue, _} | Tail], Acc) ->
+    run(Tail, [IfTrue | Acc]);
+run([{false, _, IfFalse} | Tail], Acc) ->
+    run(Tail, [IfFalse | Acc]);
 run([_ | Tail], Acc) ->
     run(Tail, Acc).
 
